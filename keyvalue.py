af9fca339753c960764feecc917fe692ef066764
Assimilationstheorie <konstantin@leafhouse.de>
2019-04-26 12:13:40 +0200

    Add KeyValue

---
+++ keyvalue.py
@@ -0,0 +1,10 @@
+country = {
+    'Deutschland': 'Berlin',
+    'Russland': 'Moskau'
+}
+
+for country, city in country.items():
+    print('Die Hauptstadt von ' + country + ' ist ' + city + '.')
+
+#Die Hauptstadt von Deutschland ist Berlin.
+#Die Hauptstadt von Russland ist Moskau.
