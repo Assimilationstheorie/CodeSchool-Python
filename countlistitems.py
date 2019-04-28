81b18eed9703609f27ba61fe348e57510aa3e82d
Assimilationstheorie <konstantin@leafhouse.de>
2019-04-28 19:42:27 +0200

    Get the whole list

--- countlistitems.py
+++ countlistitems.py
@@ -2,6 +2,8 @@ list = [
     'a', 'b', 'a', 'c', 'b', 'd', 'a', 'b'
 ]
 
+print('Die Liste = ', list)
+
 for item in sorted(set(list)):
     count = list.count(item)
     #f-string, erlaubt Str && Int
