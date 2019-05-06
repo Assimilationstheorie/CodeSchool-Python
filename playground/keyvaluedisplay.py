41d54577c02ee987b1d8df12d4706aed4cc32309
Assimilationstheorie <konstantin@leafhouse.de>
2019-05-06 17:00:13 +0200

    Add KeyValueDisplay

---
+++ playground/keyvaluedisplay.py
@@ -0,0 +1,12 @@
+data = {
+    "Germany": "Berlin",
+    "Russia": "Moskau",
+    "France": "Paris"
+}
+
+print(data.keys())
+print(data.values())
+
+# Output:
+# dict_keys(['Germany', 'Russia', 'France'])
+# dict_values(['Berlin', 'Moskau', 'Paris'])
