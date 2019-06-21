98191750f05e1de0e136ff51d98f401de5a136ea
Assimilationstheorie <konstantin@leafhouse.de>
2019-06-21 13:35:37 +0200

    Add Output to CountLetters

--- playground/countletters.py
+++ playground/countletters.py
@@ -7,3 +7,15 @@ stats = {
 for x in sorted(
     stats, key=stats.get, reverse=True):
     print(f'{x} = {stats[x]}')
+
+# Ausgabe:
+# l = 3
+# o = 2
+# H = 1
+# e = 1
+# , = 1
+#   = 1
+# W = 1
+# r = 1
+# d = 1
+# ! = 1
